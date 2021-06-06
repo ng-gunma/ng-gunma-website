95d5590487520bac3a582e920bfcfd5fc6f303fa
gurezo <akihiko.kigure@gmail.com>
2021-06-06 17:17:15 +0900

    v12 update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,11 @@
 import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AboutComponent } from './about/about.component';
-import { HomeComponent } from './home/home.component';
-
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: HomeComponent
-  },
-  {
-    path: 'about',
-    component: AboutComponent
+    component: AppComponent
   }
 ];
 
